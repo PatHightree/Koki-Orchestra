968f2260b3b9ff67735d5b157f28bb8c93b663cf
Patrick Hogenboom <patrick.hogenboom@pm.me>
2023-06-07 18:07:43 +0200

    Switch on one led at a time and process

--- Calibrate/main.py
+++ Calibrate/main.py
@@ -3,115 +3,93 @@ import random
 
 import cv2
 import numpy as np
-import time
-from datetime import datetime
-from datetime import timedelta
+from timeit import default_timer as timer
 from wled import *
 from camera import *
 
-leds_init(255)
-led_brightness = 100
+display_brightness = 100
+led_value = 70
+led_count = 64
+led_index = 0
 blur_size = 10
-brightness_multiplier = 3
+capture_brightness_multiplier = 2
 
 
-def display_sample():
-    global t, sample_index
-    if datetime.now() > t + interval:
-        if sample_index < samples_count:
-            t = datetime.now()
-            led_set(samples[sample_index], 0)
-            sample_index = sample_index + 1
-        if sample_index < samples_count:
-            led_set(samples[sample_index], led_brightness)
+def display_sequential():
+    global led_index
+    led_set(led_index, 0)
+    led_index = (led_index + 1) % led_count
+    led_set(led_index, led_value)
 
 
-def display_sample_infinite():
-    global t, sample_index
-    if datetime.now() > t + interval:
-        t = datetime.now()
-        led_set(samples[sample_index], 0)
-        sample_index = (sample_index + 1) % samples_count
-        led_set(samples[sample_index], led_brightness)
+def capture_background():
+    print("Background {}", timer())
+    ret, background = capture.read()
+    while not ret:
+        pass
+    background = cv2.blur(background, ksize=(blur_size, blur_size))
+    return background
 
 
-def display_sample_random():
-    global t, sample_index
-    if datetime.now() > t + interval:
-        t = datetime.now()
-        led_set(sample_index, 0)
-        sample_index = random.randrange(64)
-        led_set(sample_index, led_brightness)
+def capture_sample():
+    print("Sample {}", timer())
+    ret, frame = capture.read()
+    while not ret:
+        pass
+    frame_blur = cv2.blur(frame, ksize=(blur_size, blur_size))
+    return frame, frame_blur
 
-
-# list_ports()
 
 capture = cv2.VideoCapture(1, cv2.CAP_DSHOW)
-if (capture.isOpened() == False):
+if not capture.isOpened():
     print("Error opening capture stream or file")
 frame_width = int(capture.get(3))
 frame_height = int(capture.get(4))
 print(f"Video resolution={frame_width}x{frame_height}")
 
-samples = [0, 0 + 7, 63, 63 - 7]
-samples_count = len(samples)
-sample_index = 0
-interval = timedelta(seconds=1)
-
-# Prepare capture
+# capture background twice to let camera sensitivity accommodate
+leds_init(display_brightness)
 leds_off()
-time.sleep(1)
-ret, background = capture.read()
-if ret:
-    cv2.imshow("Background", background)
-leds_off()
-time.sleep(1)
-ret, background = capture.read()
-if ret:
-    cv2.imshow("Background", background)
-background = cv2.blur(background, ksize=(blur_size, blur_size))
-
-# Start capture
-led_set(samples[sample_index], led_brightness)
-t = datetime.now()
+cv2.waitKey(100)
+capture_background()
+cv2.waitKey(100)
+background_blur = capture_background()
+cv2.imshow("Background", background_blur)
+cv2.moveWindow("Background", 0, 0)
 
 detector = cv2.SimpleBlobDetector_create()
 
-
+delay = 100
 while capture.isOpened():
-    display_sample_random()
+    led_set(led_index, 0)
+    led_index = (led_index + 1) % led_count
+    led_set(led_index, led_value)
 
-    ret, frameOrg = capture.read()
-    if ret:
-
-        # Blur frame
-        frame = cv2.blur(frameOrg, ksize=(blur_size, blur_size))
-        cv2.imshow("Blur", frame)
-
-        # Subtract background
-        delta = cv2.subtract(frame, background)
-        delta = delta * brightness_multiplier
-        # delta = delta.astype('uint8')
-        cv2.imshow("Delta", delta)
-
-        # Invert image because default blob detection wants black blobs on white background
-        delta = ~delta
-
-        keypoints = detector.detect(delta)
-
-        # Draw detected blobs as red circles.
-        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-        im_with_keypoints = cv2.drawKeypoints(frameOrg, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-        # Show keypoints
-        cv2.imshow("Blobs", im_with_keypoints)
-
-        # Press escape to quit
-        if cv2.waitKey(25) & 0xFF == 27:
-            break
-
-    else:
+    if cv2.waitKey(delay) & 0xFF == 27:
         break
+
+    sample, sample_blur = capture_sample()
+    cv2.imshow("Sample", sample_blur)
+    cv2.moveWindow("Sample", frame_width, 0)
+    led_set(led_index, led_value)
+
+    # Subtract background
+    delta = cv2.subtract(sample_blur, background_blur)
+    delta = delta * capture_brightness_multiplier
+    cv2.imshow("Delta", delta)
+    cv2.moveWindow("Delta", 0, frame_height)
+
+    # Invert image because default blob detection wants black blobs on white background
+    delta = ~delta
+
+    keypoints = detector.detect(delta)
+    # Draw detected blobs as red circles.
+    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+    im_with_keypoints = cv2.drawKeypoints(sample, keypoints, np.array([]), (0, 0, 255),
+                                          cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # Show keypoints
+    cv2.imshow("Blobs", im_with_keypoints)
+    cv2.moveWindow("Blobs", frame_width, frame_height)
 
 capture.release()
 cv2.destroyAllWindows()
